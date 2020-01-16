b9bdf0398ff5ef8f78fa5086d856f79a7a8f63c2
GingerNinja <louis23412@gmail.com>
2020-01-16 21:32:35 +0200

    adding basic routing for pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Logo from './components/Logo/Logo';
 import Clarifai from 'clarifai';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Register from './components/Register/Register';
+import SignIn from './components/signIn/signIn';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import './App.css';
@@ -30,7 +32,9 @@ class App extends Component {
     this.state = {
       input : '',
       imageUrl : '',
-      box : {}
+      box : {},
+      route : 'signin',
+      isSignedIn : false
     }
   }
 
@@ -48,7 +52,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({box : box});
   }
 
@@ -63,15 +66,37 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn : false})
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions}/>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        <Navigation isSignedIn={isSignedIn} is onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ?
+          <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition box={box} imageUrl={imageUrl}/>
+          </div>
+          : (
+            route === 'signin'
+            ?
+            <SignIn onRouteChange={this.onRouteChange}/>
+            :
+            <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
       </div>
     );
   }
