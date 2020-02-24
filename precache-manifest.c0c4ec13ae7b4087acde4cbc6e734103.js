9a0e457501455f09cf5c9b8b11a61f189c669cfd
GingerNinja <louis23412@gmail.com>
2020-02-24 19:42:28 +0200

    Updates

---
+++ precache-manifest.c0c4ec13ae7b4087acde4cbc6e734103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca480e8e4b54c858bdb2bc9927915d0",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "4da38a2281c5c8351136",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1ce11f5f6399b2457fc1",
+    "url": "/smart-brain/static/css/main.f28f1324.chunk.css"
+  },
+  {
+    "revision": "4da38a2281c5c8351136",
+    "url": "/smart-brain/static/js/2.f3f08ff1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain/static/js/2.f3f08ff1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ce11f5f6399b2457fc1",
+    "url": "/smart-brain/static/js/main.ced1c0c6.chunk.js"
+  },
+  {
+    "revision": "e68a7aa19c1e5c2a2d86",
+    "url": "/smart-brain/static/js/runtime-main.4dc2cd4d.js"
+  }
+]);
